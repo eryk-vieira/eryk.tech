b2230e38d4899504caa66033ef9322ec8d5ec470
Eryk-Luiz <erykluizvieira@gmail.com>
2022-11-27 19:06:38 -0300

    Fav Icon

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex } from "@chakra-ui/react";
+import Head from "next/head";
 import { ReactNode } from "react";
 import { Footer } from "./footer";
 import { Nav } from "./nav";
@@ -10,16 +11,23 @@ type ContainerProps = {
 
 export function Container({ children }: ContainerProps) {
   return (
-    <Flex flexDir={'column'} maxW="700" justify={'center'} margin={'0 auto'} pl={12} pr={12}>
-      <Flex as={'header'} w="100%" justifyItems={'center'}>
-        <Nav />
+    <>
+      <Head>
+        <title>Eryk Vieira | Site</title>
+        <link rel="shortcut icon" href="https://ui-avatars.com/api/?name=Eryk+Vieira&background=1a202c&color=fff" />
+      </Head>
+
+      <Flex flexDir={'column'} maxW="700" justify={'center'} margin={'0 auto'} pl={12} pr={12}>
+        <Flex as={'header'} w="100%" justifyItems={'center'}>
+          <Nav />
+        </Flex>
+
+        <Box as="main" pb="16" flex={1}>
+          {children}
+        </Box>
+
+        <Footer />
       </Flex>
-
-      <Box as="main" pb="16" flex={1}>
-        {children}
-      </Box>
-
-      <Footer />
-    </Flex>
+    </>
   )
 }
